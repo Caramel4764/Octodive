97795b7adfd16fde9f2e810e59677228fb7c18a3
caramel4764 <nina.dong4764@gmail.com>
2024-08-08 17:58:05 -0700

    feat: create octopus art, add player, and can move up or down

--- script.js
+++ script.js
@@ -7,26 +7,47 @@ var config = {
       create: create,
       update: update
   },
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 0 },
+        debug: false
+    }
+},
   pixelArt: true,
 };
 
 var game = new Phaser.Game(config);
+let player;
+let cursor;
+let currLane = 1;
 
 function preload ()
 {
   this.load.image('oceanBg', 'assets/ocean.png');
-  /*this.load.spritesheet('dude',
-      'assets/dude.png',
-  { frameWidth: 32, frameHeight: 48 }
-);*/
+  this.load.spritesheet('octopus',
+      'assets/octopus.png',
+  { frameWidth: 125, frameHeight: 100 }
+);
+//this.load.image('octopus', 'assets/octopus.png');
+
 }
 
 function create ()
 {
   this.add.image(400, 300, 'oceanBg').setScale(3);
-
+  player = this.physics.add.sprite(100, 0, 'octopus');
+  player.setCollideWorldBounds(true);
+  //cursor
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update ()
 {
+  if (cursors.down.isDown) {
+      player.setVelocityY(160);
+  }
+  if (cursors.up.isDown) {
+    player.setVelocityY(-160);
+}
 }
