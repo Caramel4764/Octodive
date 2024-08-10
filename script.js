fa40b902e888a0260066092c73d69a941f853dd2
caramel4764 <nina.dong4764@gmail.com>
2024-08-10 16:35:59 -0700

    fix|feat: add silver ring into game. Adjust octopus collision box.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var config = {
+let config = {
   type: Phaser.AUTO,
   width: 900,
   height: 600,
@@ -32,12 +32,13 @@ let playerInfo = {
   playerVerticalSpeed: 400,
   ogPlayerSpeed: 2,
   isBoosting: false,
-  loopSpawnInterval: 2000,
+  goldLoopSpawnInterval: 5000,
+  silverLoopSpawnInterval: 500,
   score: 0,
   currLane: 0,
   finishedLaneSwitching: true,
 }
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 let player;
 let cursor;
 let oceanBg;
@@ -46,6 +47,7 @@ let oceanBg2;
 let oceanBgBound2;
 let goldRing;
 let gameRef;
+let silverLoops = []
 let loops = [];
 let scoreText;
 
@@ -55,6 +57,8 @@ function preload ()
   this.load.image('oceanBgGreen', 'assets/ocean-green-test.png');
   this.load.image('goldLoopBack', 'assets/gold-ring/gold-ring-back.png');
   this.load.image('goldLoopFront', 'assets/gold-ring/gold-ring-front.png');
+  this.load.image('silverLoopBack', 'assets/silver-ring/silver-ring-back.png');
+  this.load.image('silverLoopFront', 'assets/silver-ring/silver-ring-front.png');
   this.load.image('octoHitBox', 'assets/octopus/octoHitBox.png');
   this.load.spritesheet('octopus',
       'assets/octopus/octopus.png',
@@ -62,7 +66,7 @@ function preload ()
 );
 }
 
-function spawnLoop(x, lane) {
+function spawnGoldHoop(x, lane) {
   let y = lane*calculation.laneHeight;
   let goldRingBack = gameRef.add.sprite(x, y, 'goldLoopBack').setOrigin(0, 0).setScale(4.7);
   goldRingBack.setDepth(0)
@@ -78,6 +82,24 @@ function spawnLoop(x, lane) {
   }
   loops.push(goldRingInfo);
 }
+
+function spawnSilverHoop(x, lane) {
+  let y = lane*calculation.laneHeight;
+  let silverRingBack = gameRef.add.sprite(x, y, 'silverLoopBack').setOrigin(0, 0).setScale(4.7);
+  silverRingBack.setDepth(0)
+  let silverRingFront = gameRef.add.sprite(x, y, 'silverLoopFront').setOrigin(0, 0).setScale(4.7);
+  silverRingFront.setDepth(3)
+  let silverRingBound = silverRingBack.getBounds();
+  let silverRingInfo = {
+    back: silverRingBack,
+    front: silverRingFront,
+    bound: silverRingBound,
+    lane: lane,
+    hasGivenPoint: false,
+  }
+  silverLoops.push(silverRingInfo);
+}
+
 let playerContainer;
 let playerBound;
 let octoHitBoxBound;
@@ -85,7 +107,7 @@ let octoHitBox;
 function create () {
   gameRef = this;
   //ocean
-  scoreText = this.add.text(860, 0, `${playerInfo.score}`, { font:'30px Georgia', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setDepth(10);
+  scoreText = this.add.text(845, 0, `${playerInfo.score}`, { font:'30px Georgia', fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' }).setDepth(10);
   oceanBg = this.add.image(0, 0, 'oceanBg').setScale(3).setOrigin(0, 0);
   oceanBgBound = oceanBg.getBounds();
   oceanBg2 = this.add.image(900, 0, 'oceanBg').setScale(3).setOrigin(0, 0);
@@ -95,7 +117,7 @@ function create () {
 
   playerBound = player.getBounds();
   //currently invisible hitbox
-  octoHitBox = this.add.image(82, 38, 'octoHitBox').setScale(0.8).setOrigin(0, 0).setVisible(false);
+  octoHitBox = this.add.image(90, 34, 'octoHitBox').setScale(1.3).setOrigin(0, 0).setVisible(false);
   octoHitBoxBound = octoHitBox.getBounds();
   playerContainer = this.add.container(0, 0).setScale(1.5).setDepth(2);
 
@@ -112,8 +134,15 @@ function create () {
   this.physics.world.enable(playerContainer);
   setInterval(() => {
     let randomLane = Math.floor(Math.random() * 4);
-    spawnLoop(config.width, randomLane);
-  }, playerInfo.loopSpawnInterval);
+    spawnGoldHoop(config.width, randomLane);
+  }, playerInfo.goldLoopSpawnInterval);
+
+  setInterval(() => {
+    let randomLane = Math.floor(Math.random() * 4);
+    spawnSilverHoop(config.width, randomLane);
+
+  }, playerInfo.silverLoopSpawnInterval);
+  //
   //cursor
   cursors = this.input.keyboard.createCursorKeys();
 
@@ -172,20 +201,20 @@ function handleMovingForward() {
   if (oceanBg.x-playerInfo.playerSpeed <= -oceanBgBound.width) {
     oceanBgBound.x = oceanBgBound2.x-playerInfo.playerSpeed+oceanBgBound2.width;
   } else {
-    oceanBgBound.x-=playerInfo.playerSpeed;
+    oceanBgBound.x-=playerInfo.playerSpeed/2;
   }
   oceanBg.setPosition([oceanBgBound.x], [0])
 
   if (oceanBg2.x-playerInfo.playerSpeed <= -oceanBgBound2.width) {
     oceanBgBound2.x = oceanBgBound2.width-playerInfo.playerSpeed;
   } else {
-    oceanBgBound2.x-=playerInfo.playerSpeed;
+    oceanBgBound2.x-=playerInfo.playerSpeed/2;
   }
   oceanBg2.setPosition([oceanBgBound2.x], [0]);
 
   //loops
   for (let i = 0; i < loops.length; i++) {
-    if (loops[i].hasGivenPoint == false &&loops[i].lane == playerInfo.currLane && octoHitBoxBound.x+playerInfo.playerSpeed >= loops[i].bound.x && octoHitBoxBound.x+playerInfo.playerSpeed <= loops[i].bound.x+loops[i].bound.width) {
+    if (loops[i].hasGivenPoint == false && playerInfo.finishedLaneSwitching &&loops[i].lane == playerInfo.currLane && octoHitBoxBound.x+playerInfo.playerSpeed >= loops[i].bound.x && octoHitBoxBound.x+playerInfo.playerSpeed <= loops[i].bound.x+loops[i].bound.width) {
       loops[i].hasGivenPoint = true;
       playerInfo.score+=5;
       scoreText.setText(`${playerInfo.score}`);
@@ -199,8 +228,42 @@ function handleMovingForward() {
     }
     loops[i].back.setPosition([loops[i].bound.x], [loops[i].bound.y]);
     loops[i].front.setPosition([loops[i].bound.x], [loops[i].bound.y]);
-
   }
+
+  for (let i = 0; i < silverLoops.length; i++) {
+    if (silverLoops[i].hasGivenPoint == false && playerInfo.finishedLaneSwitching && silverLoops[i].lane == playerInfo.currLane && octoHitBoxBound.x+playerInfo.playerSpeed >= silverLoops[i].bound.x && octoHitBoxBound.x+playerInfo.playerSpeed <= silverLoops[i].bound.x+silverLoops[i].bound.width) {
+      silverLoops[i].hasGivenPoint = true;
+      playerInfo.score+=1;
+      scoreText.setText(`${playerInfo.score}`);
+    }
+    if (silverLoops[i].bound.x-playerInfo.playerSpeed <= -silverLoops[i].bound.width-playerInfo.playerSpeed) {
+      silverLoops[i].front.destroy();
+      silverLoops[i].back.destroy();
+      return silverLoops.shift();
+    } else {
+      silverLoops[i].bound.x-=playerInfo.playerSpeed;
+    }
+    silverLoops[i].back.setPosition([silverLoops[i].bound.x], [silverLoops[i].bound.y]);
+    silverLoops[i].front.setPosition([silverLoops[i].bound.x], [silverLoops[i].bound.y]);
+  }
+
+  /*for (let i = 0; i < silverLoops.length; i++) {
+    if (silverLoops[i].hasGivenPoint == false && silverLoops[i].lane == playerInfo.currLane && octoHitBoxBound.x+playerInfo.playerSpeed >= silverLoops[i].bound.x && octoHitBoxBound.x+playerInfo.playerSpeed <= silverLoops[i].bound.x+silverLoops[i].bound.width) {
+      silverLoops[i].hasGivenPoint = true;
+      playerInfo.score+=1;
+      scoreText.setText(`${playerInfo.score}`);
+    }
+    if (silverLoops[i].bound.x-playerInfo.playerSpeed <= -silverLoops[i].bound.width-playerInfo.playerSpeed) {
+      silverLoops[i].front.destroy();
+      silverLoops[i].back.destroy();
+      return silverLoops.shift();
+    } else {
+      silverLoops[i].bound.x-=playerInfo.playerSpeed;
+    }
+    silverLoops[i].back.setPosition([silverLoops[i].bound.x], [silverLoops[i].bound.y]);
+    silverLoops[i].front.setPosition([silverLoops[i].bound.x], [silverLoops[i].bound.y]);
+    //silverLoops
+  }*/
 }
 
 function update (){
